d694bdb4ff738ee137b0031313992e380579b307
jjernberg <jason.jernberg@gmail.com>
2021-06-02 08:41:13 -0500

    Add migration and settings

--- gistsapp/settings.py
+++ gistsapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'gists'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}

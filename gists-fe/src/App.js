3949dac508af202fb0579e3dccd022ff1e3e9fa0
jjernberg <jason.jernberg@gmail.com>
2021-06-02 12:52:37 -0500

    Add constructor and search handling function to App

--- gists-fe/src/App.js
+++ gists-fe/src/App.js
@@ -2,6 +2,28 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {username: '', gists: []};
+    this.instance = axios.create({
+      xsrfCookieName: 'csrftoken',
+      xsrfHeaderName: 'X-CSRFToken'
+    });
+    this.handleSearchByUsername = this.handleSearchByUsername.bind(this);
+  }
+
+  handleSearchByUsername(username){
+    this.instance.get('http://localhost:8000/gists/by_username/' + username + '/')
+      .then(res => {
+        this.setState({username: username, gists: res.data})
+      })
+      .catch(error => {
+        // Get to error handling if I can, but API may not return errors yet
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <Container fixed>

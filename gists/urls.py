86ea677f6ffef0777156fa407c215ea1ff2e3b51
jjernberg <jason.jernberg@gmail.com>
2021-06-02 09:06:59 -0500

    Add the URLs for the completed views

---
+++ gists/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+import gists.views as views
+
+urlpatterns = [
+    path('by_username/<str:username>/', views.by_username),
+    path('by_id/<str:gist_id>/', views.by_id),
+    path('favorite/<str:gist_id>/', views.favorite_gist),
+    path('unfavorite/<str:gist_id>/', views.remove_favorite_gist),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
